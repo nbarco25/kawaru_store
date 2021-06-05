84fd89e2b53f31d28ced71bc022f77fa1303dec9
Natalia Barco <anbarco@unicauca.edu.co>
2021-06-05 05:57:46 -0500

    jugando con variables y diccionarios

--- kawaru_store/kawaru_store/views.py
+++ kawaru_store/kawaru_store/views.py
@@ -3,5 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'index.html',{
-        #contexto
+        'titulo': 'Kawaru',
+        'message': 'Listado de productos',
+        'entero': 23,
+        'booleano': True,
+        'lista': [1,2,3,3,5,3],
+        
+        'lista-productos': [
+            {'titulo': 'Televisor', 'Precio': 2000000, 'stock': True}, #producto
+            {'titulo': 'Carro', 'Precio': 12000000, 'stock': True },
+            {'titulo': 'Computador', 'Precio': 800000, 'stock': False}
+        ]
     })
