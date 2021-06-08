7054e45d392ddb1f93e81ec770b64bfc8ab6c71a
Natalia Barco <anbarco@unicauca.edu.co>
2021-06-08 04:07:35 -0500

    obtengo la lista de los productos en mi db y la muestro desde el template index.html

--- kawaru_store/products/models.py
+++ kawaru_store/products/models.py
@@ -6,3 +6,5 @@ class Product(models.Model):
     precio = models.DecimalField(max_digits=8, decimal_places=2, default=0.0) #decimal_places (cuantos dígitos despues de la , voy a usar)
     creado_en = models.DateTimeField(auto_now_add=True) #para que se tome la fecha y hora exacta de creación desde el sist
     
+    def __str__(self): #para mostrar el nombre del producto y no el objeto
+        return self.nombre
