b55ce2cdb55424e1737281964d399e0ad1f74530
Natalia Barco <anbarco@unicauca.edu.co>
2021-06-07 22:39:44 -0500

    login template and view with messages

--- kawaru_store/kawaru_store/views.py
+++ kawaru_store/kawaru_store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate
+from django.contrib.auth import login #para generar la sesión
+from django.contrib import messages
 
 def index(request):
  
@@ -18,7 +20,30 @@ def index(request):
     })
     
     
-def login(request):
+def login_view(request):
+    print(request.method) #Visualizar en consola el método por el cual se está haciendo la petición
+    #Condicional para obtener la información que se envía en el login a través del método POST
+    if request.method == 'POST':
+        username = request.POST.get('username') #diccionario
+        password = request.POST.get('password') #El método get del diccionario si no encuentra nada retorna None
+        
+        #Autenticar a un usuario
+        
+        user = authenticate(username=username, password=password) #busca en la bd al usuario que tenga este usuario y contrasenia, si el usuario existe, retornará un objeto de tipo usuario, si no, retornará None
+        
+        if user:
+            #Generar nueva sesión
+            #Dos parámetros petición y usuario
+            login(request, user)
+            messages.success(request, 'Bienvenido {}'.format(user.username))
+            return redirect('index') #funcion para redirigir al usuario despues de autenticarse (en este caso a index)
+        else:
+            messages.error(request, 'Usuario o contraseña no válidos')
+        
+        
+        print(username)
+        print(password)
+        
     return render(request, 'users/login.html', {
         
     })
