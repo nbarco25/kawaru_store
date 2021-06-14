b7b170a5c24ce889b86e29403fd7570399ad7963
Natalia Barco <anbarco@unicauca.edu.co>
2021-06-14 01:15:36 -0500

    calcular total y subtotal y actualizarlos segun movimientos del carrito

--- kawaru_store/carts/models.py
+++ kawaru_store/carts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from users.models import User
 from products.models import Product
 from django.db.models.signals import pre_save, m2m_changed
+import decimal
 
 # Create your models here.
 
@@ -16,16 +17,37 @@ class Cart(models.Model):
     total = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
     creado_em = models.DateTimeField(auto_now_add=True)
     
+    COMISION = 0.05 #0.5%
     
     def __str__(self):
         return self.cart_id
     
+    def actualizar_total_subtotal(self):
+        self.actualizar_subtotal()
+        self.actualizar_total()
+        
+    def actualizar_subtotal(self):
+        #El subtotal es la suma del precio de todos los productos
+        self.subtotal = sum([product.precio for product in self.products.all()])
+        self.save()
+        
+    def actualizar_total(self):
+        self.total = self.subtotal + (self.subtotal * decimal.Decimal(Cart.COMISION))
+        self.save()
+            
 def set_cart_id(sender, instance, *args, **kwargs): #callback
     if not instance.cart_id: #Si el carrito no posee un id unico
         instance.cart_id = str(uuid.uuid4())
 
-def actualizar_total(sender, instance, action, *args, **kwargs):
+def actualizar_total_subtotal(sender, instance, action, *args, **kwargs):
+    #postadd despues de que un producto se agrega al carrito de compras
+    #postremove después de que un producto se elimina del carrito de compras
+    #postclear después de que el carrito de compras se limpia
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        instance.actualizar_total_subtotal() #calcular total y subtotal
+        
+    
     
 
 pre_save.connect(set_cart_id, sender=Cart)    
-    
+m2m_changed.connect(actualizar_total_subtotal,sender=Cart.products.through)
